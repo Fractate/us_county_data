0224043e920c49fcb96aa310c3579a06b9198767
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2022-09-13 14:48:42 -0400

    Base code for triggering all events created

---
+++ base_code.R
@@ -0,0 +1,15 @@
+### This code will run all of the appropriate calls later on
+
+
+# 1) Get a list of all the counties that need to be processed from the TIGER - roads map website
+
+# 2) Get population for each of the counties
+
+# 3) Get all of the roads data including length and intersections
+
+# 4) Get the total area of each of the counties
+
+# 5) Do Analysis
+
+
+## The census data align at the county level by using County FIPS Codes. This is the State Code + County Code for 5 digit number
