f6f1512fa6f688ac3b7d1d155dcd6cfc177eea6e
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2022-09-18 14:54:10 -0400

    Prepped to initialize shapefile analysis class

--- base_code.R
+++ base_code.R
@@ -15,36 +15,69 @@ library("stringr") # allows the use of "left" and "right" string manipulations
 
 library(tidyverse) # allows the usage of left_join
 
-tiger_county_list <- get_county_list("https://www2.census.gov/geo/tiger/TIGER2020/ROADS/")
-print(tiger_county_list)
+tiger_county_list <- get_county_list()
 
-## lazy way of doing this, instead of retrieving a dataframe i am converting a returned list into a dataframe
-df <- do.call(rbind, lapply(tiger_county_list, as.data.frame))
+# print(tiger_county_list)
 
-df$names
-print(df)
-data(df)
-attributes(df)
-colnames(df) 
+# tiger_county_list should now be returned as dataframe by default so the lines below are not needed
+# ## lazy way of doing this, instead of retrieving a dataframe i am converting a returned list into a dataframe
+# df <- do.call(rbind, lapply(tiger_county_list, as.data.frame))
+
+# df$names
+# print(df)
+# data(df)
+# attributes(df)
+# colnames(df) 
 
 # 2) Get population for each of the counties
 df_county_pop <- get_county_population()
 
-print(df_county_pop)
-data(df_county_pop)
-attributes(df_county_pop)
-colnames(df_county_pop) 
+# data(df_county_pop)
+# attributes(df_county_pop)
+# colnames(df_county_pop) 
 
 df_county_pop_cropped <- df_county_pop[,c("GEOID", "NAME", "value")]
 
-# 2.1) Filter for the counties that match between the list of county population and county tiger files and populate population column
-df_lj <- left_join(df, df_county_pop_cropped, by = c("X[[i]]"="GEOID"))
+# # 2.1) Filter for the counties that match between the list of county population and county tiger files and populate population column
+# df_lj <- left_join(df, df_county_pop_cropped, by = c("X[[i]]"="GEOID"))
+df_lj <- left_join(tiger_county_list, df_county_pop_cropped, by = c("X.fips_count_id."="GEOID"))
 
-print(df_lj)
+print(tail(tiger_county_list))
+print(tail(df_county_pop))
+print(tail(df_county_pop_cropped))
+print(tail(df_lj))
+
 # 3) Get all of the roads data including length and intersections
 # add a column to df_lj to house total length and intersections
+# df_lj[ , 'roads_length', 'intersections'] <- NA
+empty_cols <- c('roads_length', 'intersections')
+df_lj[ , empty_cols] <- NA
 
-# a for loop will be needed for data retrieval per each county
+# # for loop will be needed for road data retrieval per each county
+print("flag11")
+print(tail(df_lj))
+
+for(i in 1:nrow(df_lj)) {
+    # print(df_lj[i]['X.county_zip_file_name.'])
+    # print(df_lj[i][value])
+    print(df_lj[i,2])
+    # Set up new class to handle populating road lengths and number of intersections
+}
+
+# for(i in 1:ncol(df_lj)) {       # for-loop over columns
+#   print(df_lj[ , i])
+# }
+
+
+# for (x in df_lj) {
+# #   print(x["X.county_zip_file_name."])
+#     print(x[[2]])
+# }
+# print("flag12")
+# # print(tail(df_lj))
+
+# # print(class(df_lj))
+# Sys.sleep()
 
 
 # 4) Get the total area of each of the counties
--- utils/get_list_of_county_from_tiger.R
+++ utils/get_list_of_county_from_tiger.R
@@ -1,7 +1,7 @@
 # https://www2.census.gov/geo/tiger/TIGER2020/ROADS/
 
 # Function name is 
-get_county_list <- function(website_link) {
+get_county_list <- function() {
     
     html <- read_html("https://www2.census.gov/geo/tiger/TIGER2020/ROADS/")
   
@@ -21,21 +21,14 @@ get_county_list <- function(website_link) {
     # dataframe was considered in containin county id per each county. using out() instead if possible
     # df <- data.frame(first_column, second_column)
     
-
-    out <- c()
+    df <- data.frame("fips_count_id", "county_zip_file_name")
     # Beginning and ending points of the counties list is hard coded
     for(i in 3:3234){
-        a <- tiger_county_list[i,2] # returns column of zip file names found in the US Census Tiger data
-        a
-        str_sub(a, 9, 14)
+        zip_file_name <- tiger_county_list[i,2] # returns column of zip file names found in the US Census Tiger data
         # out <- c(a, str_sub(a, 9, 13))
         # out <- c(out, c(a, str_sub(a, 9, 13)))
-        out <- c(out, str_sub(a, 9, 13))
+        df[nrow(df) + 1,] = c(str_sub(zip_file_name, 9, 13), zip_file_name)
     }
-    class(out)
-    head(out)
-    tail(out)
-    View(out)
-  
-    return(out)
+    
+    return(df)
 }
