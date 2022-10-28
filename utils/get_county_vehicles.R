4f98ff90bf58a39dfbf4621c38f1cd3ec4f1bafd
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2022-10-28 13:54:00 -0400

    checkpoint

--- utils/get_county_vehicles.R
+++ utils/get_county_vehicles.R
@@ -22,50 +22,23 @@ get_county_vehicles <- function() {
   # https://geodacenter.github.io/opioid-environment-toolkit/getACSData-tutorial.html
   # https://walker-data.com/tidycensus/reference/get_acs.html
   
-  # list of variables to potentially use
-    # DP04	SELECTED HOUSING CHARACTERISTICS # contains vehicle information
-    # DP05	ACS DEMOGRAPHIC AND HOUSING ESTIMATES
+  ### List of variables to potentially use
+  # initial variables pulled by 2020_DataProductList_5Year.xlsx
+  # secondary variables pulled by ACS2020_Table_Shells.xlsx
+  
+###############################################################################################################
     # S0801	COMMUTING CHARACTERISTICS BY SEX
-    # S1301	FERTILITY
-    # S1501	EDUCATIONAL ATTAINMENT
-    # S1701	POVERTY STATUS IN THE PAST 12 MONTHS
-    # S1702	POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES
-    # S1101	HOUSEHOLDS AND FAMILIES
-    # S1401	SCHOOL ENROLLMENT
-    # S1901	INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)
-    # S1902	MEAN INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)
-    # S1903	MEDIAN INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)
-    # S2302	EMPLOYMENT CHARACTERISTICS OF FAMILIES
-    # S2303	WORK STATUS IN THE PAST 12 MONTHS
     # S2402	OCCUPATION BY SEX FOR THE FULL-TIME, YEAR-ROUND CIVILIAN EMPLOYED POPULATION 16 YEARS AND OVER  
-    # S2602	CHARACTERISTICS OF THE GROUP QUARTERS POPULATION BY GROUP QUARTERS TYPE (3 TYPES)
-
-
-    ### ACS Data I might actually use
-    # B01001	SEX BY AGE
-    # B01003	TOTAL POPULATION
     # B07001	GEOGRAPHICAL MOBILITY IN THE PAST YEAR BY AGE FOR CURRENT RESIDENCE IN THE UNITED STATES
     # B07009	GEOGRAPHICAL MOBILITY IN THE PAST YEAR BY EDUCATIONAL ATTAINMENT FOR CURRENT RESIDENCE IN THE UNITED STATES
     # B07010	GEOGRAPHICAL MOBILITY IN THE PAST YEAR BY INDIVIDUAL INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS) FOR CURRENT RESIDENCE IN THE UNITED STATES
-    # S2501	OCCUPANCY CHARACTERISTICS
-    # S2502	DEMOGRAPHIC CHARACTERISTICS FOR OCCUPIED HOUSING UNITS
-    # S2503	FINANCIAL CHARACTERISTICS
-    # S2801	TYPES OF COMPUTERS AND INTERNET SUBSCRIPTIONS
-    # B08121	MEDIAN EARNINGS IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS) BY MEANS OF TRANSPORTATION TO WORK
-    # B08133	AGGREGATE TRAVEL TIME TO WORK (IN MINUTES) OF WORKERS BY TIME OF DEPARTURE TO GO TO WORK
-    # B09001	POPULATION UNDER 18 YEARS BY AGE 
     # B11001	HOUSEHOLD TYPE (INCLUDING LIVING ALONE)
     # B11011	HOUSEHOLD TYPE BY UNITS IN STRUCTURE
-    # B11012	HOUSEHOLDS BY TYPE
-    # B11016	HOUSEHOLD TYPE BY HOUSEHOLD SIZE
-    # B12007	MEDIAN AGE AT FIRST MARRIAGE
-    # B12503	DIVORCES IN THE LAST YEAR BY SEX BY MARITAL STATUS FOR THE POPULATION 15 YEARS AND OVER
-    # B13002	WOMEN 15 TO 50 YEARS WHO HAD A BIRTH IN THE PAST 12 MONTHS BY MARITAL STATUS AND AGE
-    # B14001	SCHOOL ENROLLMENT BY LEVEL OF SCHOOL FOR THE POPULATION 3 YEARS AND OVER
-    # B15001	SEX BY AGE BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 18 YEARS AND OVER
-    # B15002	SEX BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER
 
+    # B08133	AGGREGATE TRAVEL TIME TO WORK (IN MINUTES) OF WORKERS BY TIME OF DEPARTURE TO GO TO WORK
+###############################################################################################################
 
+###############################################################################################################
     #### initial research will exclusively be in relations to non-race specific data
     # it looks like people of color drive less
     # later researcb will include racial data
@@ -80,6 +53,109 @@ get_county_vehicles <- function() {
     # B08105I	MEANS OF TRANSPORTATION TO WORK (HISPANIC OR LATINO)
     # B08111	MEANS OF TRANSPORTATION TO WORK BY CITIZENSHIP STATUS
     # B08113	MEANS OF TRANSPORTATION TO WORK BY LANGUAGE SPOKEN AT HOME AND ABILITY TO SPEAK ENGLISH
+###############################################################################################################
+
+
+###############################################################################################################
+    ##### ACS Data I might actually use
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S2602.html
+    # S2602	CHARACTERISTICS OF THE GROUP QUARTERS POPULATION BY GROUP QUARTERS TYPE (3 TYPES)
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S2302.html
+    # S2302	EMPLOYMENT CHARACTERISTICS OF FAMILIES
+    
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S2303.html
+    # S2303	WORK STATUS IN THE PAST 12 MONTHS
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1902.html
+    # S1902	MEAN INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1903.html
+    # S1903	MEDIAN INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1901.html
+    # S1901	INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1101.html
+    # S1101	HOUSEHOLDS AND FAMILIES
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1401.html
+    # S1401	SCHOOL ENROLLMENT
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1701.html
+    # S1701	POVERTY STATUS IN THE PAST 12 MONTHS
+    # S1702	POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1301.html
+    # S1301	FERTILITY
+
+    ### https://api.census.gov/data/2017/acs/acs5/subject/groups/S1501.html
+    # S1501	EDUCATIONAL ATTAINMENT
+
+    ### https://api.census.gov/data/2019/acs/acs1/subject/groups/S2501.html
+    # S2501	OCCUPANCY CHARACTERISTICS
+    ## NOT FOUND
+    # S2502	DEMOGRAPHIC CHARACTERISTICS FOR OCCUPIED HOUSING UNITS
+    ## NOT FOUND
+    # S2503	FINANCIAL CHARACTERISTICS
+    ## NOT FOUND
+    # S2801	TYPES OF COMPUTERS AND INTERNET SUBSCRIPTIONS
+    ## NOT FOUND
+
+    # DP04	SELECTED HOUSING CHARACTERISTICS # contains vehicle information
+    ## https://api.census.gov/data/2018/acs/acs5/profile/variables.html
+    # DP05	ACS DEMOGRAPHIC AND HOUSING ESTIMATES
+    ## https://api.census.gov/data/2018/acs/acs5/profile/variables.html
+
+    # B01001	SEX BY AGE
+    ## B01001_002	Male:
+    ## B01001_026	Female:
+
+    # B01003	TOTAL POPULATION
+    ## B01003_001	Total
+    
+    # B08121	MEDIAN EARNINGS IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS) BY MEANS OF TRANSPORTATION TO WORK
+    ## B08121_001	Total:
+    ## B08121_002	Car, truck, or van - drove alone
+    ## B08121_003	Car, truck, or van - carpooled
+    ## B08121_004	Public transportation (excluding taxicab)
+    ## B08121_005	Walked
+    ## B08121_006	Taxicab, motorcycle, bicycle, or other means
+    ## B08121_007	Worked from home
+
+    # B09001	POPULATION UNDER 18 YEARS BY AGE 
+    ## B09001_001	Total:
+    ## additional age range specifics can be foudn
+
+    # B11012	HOUSEHOLDS BY TYPE
+
+    # B11016	HOUSEHOLD TYPE BY HOUSEHOLD SIZE
+    
+    # B12007	MEDIAN AGE AT FIRST MARRIAGE
+    ## B12007_001	Male
+    ## B12007_002	Female
+
+    # B12503	DIVORCES IN THE LAST YEAR BY SEX BY MARITAL STATUS FOR THE POPULATION 15 YEARS AND OVER
+    ## B12503_002	Male:
+    ## B12503_003	Never married
+    ## B12503_004	Ever married:
+    ## B12503_007	Female: 
+    ## B12503_008	Never married
+    ## B12503_009	Ever married:
+    
+    # B13002	WOMEN 15 TO 50 YEARS WHO HAD A BIRTH IN THE PAST 12 MONTHS BY MARITAL STATUS AND AGE
+    ## B13002_002	Women who had a birth in the past 12 months:
+
+    # B14001	SCHOOL ENROLLMENT BY LEVEL OF SCHOOL FOR THE POPULATION 3 YEARS AND OVER
+    ## B14001_001	Total:
+    ## B14001_002	Enrolled in school:
+
+    # B15001	SEX BY AGE BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 18 YEARS AND OVER
+    
+    # B15002	SEX BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER
+###############################################################################################################
+
 
 
     if(!file.exists("get_county_vehicles.csv")){
